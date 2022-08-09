0b2d7b630b6a7f633aefcd5c581bdb8ae30c0014
JuanDiegoAlvarado <juandiegoalvaradp@gmail.com>
2022-08-09 08:46:45 -0600

    prueba

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -28,7 +28,8 @@ exports.getAllItemsHandler = async (event) => {
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify(items),
+        res:"usuarios"
     };
 
     // All log statements are written to CloudWatch
